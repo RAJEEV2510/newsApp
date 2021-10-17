670d5ad9695f10369b5fb8124b4369c6745a4d7f
Rajeev Kumar <49456702+RAJEEV2510@users.noreply.github.com>
2021-10-18 00:29:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopheadlineComponent } from './topheadline/topheadline.component';
+import {HttpClientModule} from "@angular/common/http"
+import { TcnewsapiService } from './service/tcnewsapi.service';
+import { TechComponent } from './tech/tech.component';
+import { SportComponent } from './sport/sport.component';
+import { BussinesComponent } from './bussines/bussines.component';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopheadlineComponent,
+    TechComponent,
+    SportComponent,
+    BussinesComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:"",component:TopheadlineComponent},
+      {path:"tech",component:TechComponent},
+      {path:"sport",component:SportComponent},
+      {path:"bussiness",component:BussinesComponent}
+    ]),
+    LoadingBarHttpClientModule
   ],
-  providers: [],
+  providers: [  TcnewsapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
